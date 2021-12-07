64bb53d15c983f82b292b5f21d0caf4fc1df5863
RoisyalBariz <74692525+RoisyalBariz@users.noreply.github.com>
2021-12-07 14:25:38 +0700

    New

--- application/views/partial/Dashboard/Pegawai.php
+++ application/views/partial/Dashboard/Pegawai.php
@@ -1,26 +1,29 @@
-  <br>
-  <div class="scene">
-    <div class="carousel">
-      <div class="carousel__cell">1</div>
-      <div class="carousel__cell">2</div>
-      <div class="carousel__cell">3</div>
-      <div class="carousel__cell">4</div>
-      <div class="carousel__cell">5</div>
-      <div class="carousel__cell">6</div>
-      <div class="carousel__cell">7</div>
-      <div class="carousel__cell">8</div>
-      <div class="carousel__cell">9</div>
-    </div>
+<div class="container">
+  <center>
+    <h2>Selamat Datang di</h2>
+    <h2>Layanan Aspirasi Masyarakat Kota Bandung</h2>
+  </center>
+</div>
+<br>
+<div class="scene">
+  <div class="carousel">
+    <div class="carousel__cell">1</div>
+    <div class="carousel__cell">2</div>
+    <div class="carousel__cell">3</div>
+    <div class="carousel__cell">4</div>
+    <div class="carousel__cell">5</div>
+    <div class="carousel__cell">6</div>
+    <div class="carousel__cell">7</div>
+    <div class="carousel__cell">8</div>
+    <div class="carousel__cell">9</div>
   </div>
-  <p style="text-align: center;">
-    <button class="previous-button">Previous</button>
-    <button class="next-button">Next</button>
-  </p>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
+</div>
+<p style="text-align: center;">
+  <button class="previous-button">Previous</button>
+  <button class="next-button">Next</button>
+</p>
+<br>
+<br>
+<br>
+<br>
+<br>
